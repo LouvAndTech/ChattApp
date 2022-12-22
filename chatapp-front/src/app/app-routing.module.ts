a61f02f9c260dac24d973db1ebbc808eb8a72771
Elouan Lerissel <elouan.lerissel@gmail.com>
2022-12-22 17:31:18 +0100

    remove the routing problem

--- chatapp-front/src/app/app-routing.module.ts
+++ chatapp-front/src/app/app-routing.module.ts
@@ -3,16 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
 import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [
-  {
-    path: '',
-    component: AuthComponent,
-  },
-  {
-    path: 'home',
-    component: HomeComponent,
-  },
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
