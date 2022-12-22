b06c39b9da1326fdc6110976118a2eb4f5283c45
Elouan Lerissel <elouan.lerissel@gmail.com>
2022-12-22 20:07:13 +0100

    cat images get download and assign automatically in the back

--- chatapp-back/main.go
+++ chatapp-back/main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
+	"io"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
 	"path/filepath"
 	"strings"
@@ -34,6 +39,24 @@ func main() {
 		"the directory to serve static files",
 	)
 
+	app.OnModelAfterCreate().Add(func(e *core.ModelEvent) error {
+		log.Println(e.Model.TableName())
+		if e.Model.TableName() == "users" {
+			log.Println("users created")
+			record, err := app.Dao().FindRecordById("users", e.Model.GetId())
+			if err != nil {
+				return err
+			}
+
+			record.Set("avatar", getCatImg(e.Model.GetId()))
+
+			if err := app.Dao().SaveRecord(record); err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+
 	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
 		// serves static files from the provided public dir (if exists)
 		e.Router.GET("/*", apis.StaticDirectoryHandler(os.DirFS(publicDirFlag), false))
@@ -43,4 +66,68 @@ func main() {
 	if err := app.Start(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+type CatImg struct {
+	Id     string `json:"id"`
+	Url    string `json:"url"`
+	With   int    `json:"width"`
+	Height int    `json:"height"`
+}
+
+func getCatImg(userId string) string {
+	response, err := http.Get("https://api.thecatapi.com/v1/images/search")
+	if err != nil {
+		return "nil"
+	}
+
+	data, _ := ioutil.ReadAll(response.Body)
+
+	var rep []CatImg
+
+	err = json.Unmarshal(data, &rep)
+	if err != nil {
+		log.Println(err)
+	}
+
+	path, _ := os.Getwd()
+
+	if err := os.MkdirAll("pb_data/storage/_pb_users_auth_/"+userId, os.ModePerm); err != nil {
+		log.Fatal(err)
+	}
+
+	err = downloadFile(rep[0].Url, path+"/pb_data/storage/_pb_users_auth_/"+userId+"/cat"+rep[0].Id+".jpg")
+	if err != nil {
+		log.Println(err)
+	}
+
+	return ("cat" + rep[0].Id + ".jpg")
+
+}
+
+func downloadFile(URL, fileName string) error {
+	//Get the response bytes from the url
+	response, err := http.Get(URL)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != 200 {
+		return errors.New("Received non 200 response code")
+	}
+	//Create a empty file
+	file, err := os.Create(fileName)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	//Write the bytes to the fiel
+	_, err = io.Copy(file, response.Body)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
