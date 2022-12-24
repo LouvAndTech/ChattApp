931f92e1709db51a2e222af1c1d1ad45fca5e084
Elouan Lerissel <elouan.lerissel@gmail.com>
2022-12-24 13:19:44 +0100

    Add more choice to build

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ npm install
 ng build --configuration=production
 cd ..
 #Build for active Arch
-#docker build --tag louvandtech/chat-app .
+#docker build --tag louvandtech/chat-app -f Dockerfile.noAngular .
 #Build for Multi-Arch
-docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t louvandtech/chat-app --push .
-echo("Compliation Done")
+docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t louvandtech/chat-app --push -f Dockerfile.noAngular .
+echo ">>>Compliation Done !"
